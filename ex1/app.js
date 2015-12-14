46a31ace848d868f4cf8630d7b53d72bb02586e5
weejh <wee_jh@yahoo.com>
2015-12-14 18:36:45 +0800

    update with selection

--- ex1/app.js
+++ ex1/app.js
@@ -14,14 +14,6 @@ document.addEventListener('click', event => {
   }
 })
 
-document.addEventListener('change', event => {
-  // console.dir(event.target.value)
-  // console.log(window.location.pathname + '/?' + event.target.value)
-  window.history.pushState(null, '', window.location.pathname + '?' + document.querySelector('#color').value + '+' + document.querySelector('#size').value)
-  // window.location.replace(window.location.pathname + '/?' + event.target.value)
-  // route()
-})
-
 function route () {
   Array.from(document.querySelectorAll('section'))
     .forEach(section =>
@@ -44,3 +36,29 @@ function route () {
 }
 
 route()
+// Array.from(document.querySelectorAll('nav a'))
+//   .forEach(anchor =>
+//     anchor.addEventListener('click', event => {
+//       event.preventDefault()
+//       // console.dir(anchor)
+//       window.history.pushState(null, '', anchor.attributes.href.value)
+//       Array.from(document.querySelectorAll('section'))
+//         .forEach(section =>
+//           section.style.display = 'none'
+//         )
+//       switch (anchor.attributes.href.value) {
+//         case '/about':
+//           document.title = 'About us'
+//           document.querySelector('#about')
+//             .style.display = 'block'
+//           break
+//         case '/products':
+//           document.title = 'products'
+//           document.querySelector('#products')
+//             .style.display = 'block'
+//           break
+//         case '/':
+//           break
+//       }
+//     })
+//   )
