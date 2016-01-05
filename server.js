d1aff21bc298f08c83e1eb9a91f524337798a1a1
weejh <wee_jh@yahoo.com>
2016-01-05 22:14:40 +0800

    ssl

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var fallback = require('express-history-api-fallback')
 var express = require('express')
 var app = express()
+// for https
+var fs = require('fs')
+var https = require('https')
+https.createServer({
+  key: fs.readFileSync('./ssl-keys/private.key'),
+  cert: fs.readFileSync('./ssl-keys/certificate.pem')
+}, app).listen(8080)
+//
 var root = __dirname + '/'
 app.use(express.static(root))
 app.use(fallback('index.html', { root: root }))
-app.listen(8080)
+// require for http
+// app.listen(8080)
