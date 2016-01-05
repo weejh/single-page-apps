a09988ff329d2ebfe401d59709c13136c710738e
weejh <wee_jh@yahoo.com>
2016-01-05 21:56:17 +0800

    updated

--- app.js
+++ app.js
@@ -1,52 +1,24 @@
-window.addEventListener('popstate', event => {
-  console.log(event)
-  route()
-})
 
-document.addEventListener('click', event => {
-  if (event.target.tagName === 'A') {
-    if (event.target.href.startsWith(window.location.origin)) {
-      event.preventDefault()
-      console.log(event)
-      window.history.pushState(null, '', event.target.attributes.href.value)
-      route()
-    }
-  }
-})
+var Router = require('spa-route')
 
-document.addEventListener('change', event => {
-  // console.dir(event.target.value)
-  // console.log(window.location.pathname)
-  if (window.location.pathname === '/products') {
-    window.history.replaceState(null, '', window.location.pathname + '?' + document.querySelector('#color').value + '+' + document.querySelector('#size').value)
+var router = new Router([
+  {
+    route: /^\/$/,
+    title: 'Homepage',
+    element: '#landing'
+  },
+  {
+    route: /^\/about/,
+    title: 'About Us',
+    element: '#about'
+  },
+  {
+    route: /^\/products/,
+    title: 'products',
+    element: '#products'
+  },
+  {
+    route: /^$/,
+    element: '#error404'
   }
-  if (window.location.pathname === '/about') {
-      // console.log(window.location.pathname)
-      // console.log(document.querySelector('#group').value)
-    window.history.replaceState(null, '', window.location.pathname + '?' + document.querySelector('#group').value)
-  }
-  // route()
-})
-
-function route () {
-  Array.from(document.querySelectorAll('section'))
-    .forEach(section =>
-        section.style.display = 'none'
-      )
-  switch (window.location.pathname) {
-    case '/about':
-      document.title = 'About us'
-      document.querySelector('#about')
-        .style.display = 'block'
-      break
-    case '/products':
-      document.title = 'products'
-      document.querySelector('#products')
-        .style.display = 'block'
-      break
-    case '/':
-      break
-  }
-}
-
-route()
+])
